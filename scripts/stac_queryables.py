55091a2066ec07059f8bc8e3b0bb1d0d12dfaa5f
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2026-04-13 17:51:57 +0200

    Update stac_queryables.py

--- scripts/stac_queryables.py
+++ scripts/stac_queryables.py
@@ -1,6 +1,6 @@
 from stac_mcp.tools.client import STACClient
 
-client = STACClient(catalog_url="https://planetarycomputer.microsoft.com/api/stac/v1")
+client = STACClient(catalog_url="https://data.geo.admin.ch/api/stac/v1/")
 res = client.get_queryables(collection_id="sentinel-2-l2a")
 print(res.keys())
 print(list(res.get("queryables", {}).keys())[:20])
